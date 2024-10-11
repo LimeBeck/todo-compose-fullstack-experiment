a0ce40b2b3393fb24c8ed103c88c764f14ac896f
Anatoly Nechay-Gumen <mail@limebeck.dev>
2024-10-11 12:09:08 +0000

    Format, move package

--- app/common/src/commonMain/kotlin/dev/limebeck/todo/TodoService.kt
+++ app/common/src/commonMain/kotlin/dev/limebeck/todo/TodoService.kt
@@ -1,24 +1,10 @@
+package dev.limebeck.todo
+
+import kotlin.jvm.JvmInline
 import kotlinx.coroutines.flow.Flow
 import kotlinx.rpc.RPC
 import kotlinx.serialization.Serializable
-import kotlin.jvm.JvmInline
 
-@Serializable 
-@JvmInline
-value class TaskId(val value: String)
-
-@Serializable
-data class Task(
-        val id: TaskId,
-        val description: String,
-        val completed: Boolean,
-)
-
-@Serializable 
-data class NewTask(val description: String)
-
-@Serializable
-data class TasksFilter(val completed: Boolean? = null)
 
 interface TodoService : RPC {
     suspend fun addTask(task: NewTask): Task
@@ -28,3 +14,20 @@ interface TodoService : RPC {
 
     suspend fun subscribeToNewTasks(): Flow<Task>
 }
+
+@Serializable 
+@JvmInline 
+value class TaskId(val value: String)
+
+@Serializable
+data class Task(
+    val id: TaskId,
+    val description: String,
+    val completed: Boolean,
+)
+
+@Serializable 
+data class NewTask(val description: String)
+
+@Serializable 
+data class TasksFilter(val completed: Boolean? = null)
